c8814b7f6f0b6c47c89fea6074bd49e0847deeda
luisiJavo <luis.aranda.lillo@gmail.com>
2022-03-25 13:09:55 +0100

    rename relecov to relecov_platform, rename dashboard to relecov_core

--- received_samples/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def received_samples(request):
-    context = {}
-    return render(request, 'received_samples/received_samples.html', context)
