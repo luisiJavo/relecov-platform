4c1cfc8561392a400564d0ae1405f3823fb29bb1
luisiJavo <luis.aranda.lillo@gmail.com>
2022-04-04 11:25:17 +0200

    04-04-2022, view readtest modified

--- relecov_platform/urls.py
+++ relecov_platform/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('relecov_core.urls')),
+    path('dashboard/', include('relecov_dashboard.urls')),
     
 ]
