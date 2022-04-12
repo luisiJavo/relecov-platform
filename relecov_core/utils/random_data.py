b25077aa49dbb0b93bd9691d5301a6209af8d223
birbi <luis.aranda.lillo@gmail.com>
2022-04-12 21:28:06 +0200

    pull resuelto conflictos

--- relecov_core/utils/random_data.py
+++ relecov_core/utils/random_data.py
@@ -3,11 +3,7 @@ import random
 
 def generate_random_sequences():
     sequence_list = []
-<<<<<<< HEAD
-    for i in range(3886):
-=======
     for i in range(103):
->>>>>>> 6e58e6a75e143d94e970db494e5028a11c37c592
         sequence = random.randint(100, 1000)
         sequence_list.append(sequence)
     return sequence_list
@@ -16,29 +12,6 @@ def generate_random_sequences():
 def generate_weeks():
     weeks_list = []
 
-<<<<<<< HEAD
-    for j in range(200):
-        weeks_list.append(1)
-    for k in range(300):
-        weeks_list.append(2)
-    for l in range(400):
-        weeks_list.append(3)
-    for m in range(375):
-        weeks_list.append(4)
-    for n in range(386):
-        weeks_list.append(5)
-    for o in range(400):
-        weeks_list.append(6)
-    for p in range(450):
-        weeks_list.append(7)
-    for q in range(500):
-        weeks_list.append(8)
-    for r in range(400):
-        weeks_list.append(9)
-    for s in range(350):
-        weeks_list.append(10)
-    for t in range(125):
-=======
     for j in range(10):
         weeks_list.append(1)
     for k in range(10):
@@ -60,7 +33,6 @@ def generate_weeks():
     for s in range(10):
         weeks_list.append(10)
     for t in range(3):
->>>>>>> 6e58e6a75e143d94e970db494e5028a11c37c592
         weeks_list.append(11)
 
     return weeks_list
