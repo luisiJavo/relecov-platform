789e32d9c2b56b709b7c2558f2fad53acbf904c0
luisiJavo <luis.aranda.lillo@gmail.com>
2022-05-06 11:05:42 +0200

    flake error reduction

--- manage.py
+++ manage.py
@@ -7,8 +7,8 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault(
-        'DJANGO_SETTINGS_MODULE', 
-        'relecov_platform.settings'
+    'DJANGO_SETTINGS_MODULE',
+    'relecov_platform.settings'
     )
     try:
         from django.core.management import execute_from_command_line
