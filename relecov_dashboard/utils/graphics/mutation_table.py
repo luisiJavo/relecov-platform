db6dfe43c4b5db22c830dee3b7ed8ffcc2b262b7
Pablo-Aja-Macaya <paabloaja@gmail.com>
2022-06-29 23:32:58 +0200

    Adapted to linting

--- relecov_dashboard/utils/graphics/mutation_table.py
+++ relecov_dashboard/utils/graphics/mutation_table.py
@@ -19,24 +19,25 @@ import pandas as pd
 import json
 
 
-def read_data(input_file: str, file_extension: str = 'csv') -> pd.DataFrame:
+def read_data(input_file: str, file_extension: str = "csv") -> pd.DataFrame:
     """
     Read data, either in CSV or JSON format.
     If in JSON format, the JSON must follow a structure of [{'pk': {'atr1':'z'} }]
     Returns a pandas dataframe object
     """
     df = None
-    if file_extension == 'json':
+    if file_extension == "json":
         with open(input_file) as f:
             # JSON must have a "primary key", which is the sample ID
-            df = pd.DataFrame.from_dict(json.load(f), orient='index') 
-            df = df.reset_index().rename(columns={'index':'SAMPLE'})
-    elif file_extension == 'csv':
-        df = pd.read_csv(input_file, sep=',')
+            df = pd.DataFrame.from_dict(json.load(f), orient="index")
+            df = df.reset_index().rename(columns={"index": "SAMPLE"})
+    elif file_extension == "csv":
+        df = pd.read_csv(input_file, sep=",")
     else:
-        raise Exception('Unrecognized file format!')
+        raise Exception("Unrecognized file format!")
 
     return df
+
 
 def process_df(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -44,18 +45,19 @@ def process_df(df: pd.DataFrame) -> pd.DataFrame:
     (maybe usefull for translations)
     """
     translate_dicc = {
-        'SAMPLE':'SAMPLE',
-        'POS':'POS',
-        'AF':'AF',
-        'EFFECT': 'EFFECT',
-        'GENE':'GENE',
-        'LINEAGE': 'LINEAGE'
+        "SAMPLE": "SAMPLE",
+        "POS": "POS",
+        "AF": "AF",
+        "EFFECT": "EFFECT",
+        "GENE": "GENE",
+        "LINEAGE": "LINEAGE",
     }
     # Rename and select columns
-    df = df[translate_dicc.keys()] # select specific keys
-    df = df.rename(columns = translate_dicc) # rename to the required output names
+    df = df[translate_dicc.keys()]  # select specific keys
+    df = df.rename(columns=translate_dicc)  # rename to the required output names
 
     return df
+
 
 def create_mutation_table(sample):
     pass
@@ -63,27 +65,29 @@ def create_mutation_table(sample):
 
 # ---- Set up ----
 # Input
-input_file = '/home/usuario/Proyectos/relecov/relecov-platform/data/variants_long_table.csv'
+input_file = (
+    "/home/usuario/Proyectos/relecov/relecov-platform/data/variants_long_table.csv"
+)
 sample_id = "214821"
 
 # Read data
-df = read_data(input_file, file_extension='csv')
+df = read_data(input_file, file_extension="csv")
 df = process_df(df)
 
 # Read some extra values
-effects = list(df['EFFECT'].unique())
-sample_ids = list(df['SAMPLE'].unique())
+effects = list(df["EFFECT"].unique())
+sample_ids = list(df["SAMPLE"].unique())
 
 
 # ---- Dash app ----
 app = dash.Dash(__name__)
 
 app.layout = html.Div(
-    children = [
-        html.P(id='mutation_table-message'),
+    children=[
+        html.P(id="mutation_table-message"),
         dcc.Dropdown(
             id="mutation_table-effect_dropdown",
-            options = [{'label':i, 'value': i} for i in effects],
+            options=[{"label": i, "value": i} for i in effects],
             clearable=False,
             multi=True,
             value=1,
@@ -101,12 +105,12 @@ app.layout = html.Div(
         html.Br(),
         dash_table.DataTable(
             id="mutation_table",
-            data = df.to_dict('records'), 
-            columns = [{"name": i, "id": i} for i in df.columns]
+            data=df.to_dict("records"),
+            columns=[{"name": i, "id": i} for i in df.columns],
         ),
-
     ]
 )
+
 
 @app.callback(
     Output("mutation_table", "data"),
@@ -115,9 +119,9 @@ app.layout = html.Div(
 def update_selected_effects(selected_effects):
     data = df
     if type(selected_effects) == list and len(selected_effects) >= 1:
-        data = data[data['EFFECT'].isin(selected_effects)]
-    
-    return data.to_dict('records')
+        data = data[data["EFFECT"].isin(selected_effects)]
+    return data.to_dict("records")
+
 
 # @app.callback(
 #     Output("mutation_table", "data"),
@@ -127,15 +131,18 @@ def update_selected_effects(selected_effects):
 #     data = df
 #     if type(selected_sample) == str:
 #         data = data[data['SAMPLE'].isin([selected_sample])]
-    
 #     return data.to_dict('records')
 
-@app.callback(Output('mutation_table-message', 'children'), Input('mutation_table', 'active_cell'))
+
+@app.callback(
+    Output("mutation_table-message", "children"), Input("mutation_table", "active_cell")
+)
 def show_clicks(active_cell):
     if active_cell:
-        return str(active_cell)  
-    else: 
+        return str(active_cell)
+    else:
         return "Click the table"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run_server(debug=True)
