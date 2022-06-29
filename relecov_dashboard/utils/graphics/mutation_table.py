65bdf911056a78b9aadf61d1ea3e0334135b6deb
Pablo Aja Macaya <pablo.aja@udc.es>
2022-06-29 15:42:41 +0200

    Added libraries and to do list

--- relecov_dashboard/utils/graphics/mutation_table.py
+++ relecov_dashboard/utils/graphics/mutation_table.py
@@ -1,4 +1,24 @@
-# Init
+# Dash libs
+import dash
+import dash_table
+from dash.dependencies import Input, Output
+import dash_bio as dashbio
+import dash_core_components as dcc
+import dash_html_components as html
 
+# Other libs
+import pandas as pd
+import json
+import urllib.request as urlreq
 
-# Hola Pablo
+"""
+Mutaciones asociadas a linajes, con sus frecuencias
+
+- Generar script
+- Leer json/csv
+- Generar dataframe
+- Tabla auxiliar al needle plot
+    - needle plot encima
+    - debajo una tabla para que se pueda filtrar, viendo frecuencia de una mutación en linaje
+    
+"""
