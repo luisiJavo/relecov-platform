09beca52af8cef79342eb77bf557dd18a65c5eac
Pablo Aja Macaya <pablo.aja@udc.es>
2022-06-29 15:59:44 +0200

    Testing linter

--- relecov_dashboard/utils/graphics/mutation_table.py
+++ relecov_dashboard/utils/graphics/mutation_table.py
@@ -1,16 +1,3 @@
-# Dash libs
-import dash
-import dash_table
-from dash.dependencies import Input, Output
-import dash_bio as dashbio
-import dash_core_components as dcc
-import dash_html_components as html
-
-# Other libs
-import pandas as pd
-import json
-import urllib.request as urlreq
-
 """
 Mutaciones asociadas a linajes, con sus frecuencias
 
@@ -20,5 +7,4 @@ Mutaciones asociadas a linajes, con sus frecuencias
 - Tabla auxiliar al needle plot
     - needle plot encima
     - debajo una tabla para que se pueda filtrar, viendo frecuencia de una mutación en linaje
-    
 """
