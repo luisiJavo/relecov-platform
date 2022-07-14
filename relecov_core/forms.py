5c6227f61dd0a3c6f40d61bd1fd5364e2160f8f1
luisiJavo <luis.aranda.lillo@gmail.com>
2022-07-14 17:12:05 +0200

    markdown testing

---
+++ relecov_core/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import MarkDownModel
+
+
+class MarkedDownExampleForm(forms.ModelForm):
+    class Meta:
+        
+        model = MarkDownModel
+        fields = '__all__'
