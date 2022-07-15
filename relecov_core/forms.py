046d0152fa6eb88f7ab160613f6cbf3b0dcbae1d
birbi <luis.aranda.lillo@gmail.com>
2022-07-15 08:19:02 +0200

    markdown test ok

--- relecov_core/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import MarkDownModel
-
-
-class MarkedDownExampleForm(forms.ModelForm):
-    class Meta:
-
-        model = MarkDownModel
-        fields = "__all__"
