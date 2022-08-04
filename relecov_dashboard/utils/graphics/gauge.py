f6ca2ed52670bb160e284fbab0099277920d01f0
luisiJavo <luis.aranda.lillo@gmail.com>
2022-08-04 13:50:47 +0200

    relecov_core/api/-> created update_state function, relecov_core/models.py created Error class, modified Sample class

--- relecov_dashboard/utils/graphics/gauge.py
+++ relecov_dashboard/utils/graphics/gauge.py
@@ -4,17 +4,37 @@ import dash_core_components as dcc
 import dash_html_components as html
 from django_plotly_dash import DjangoDash
 
+import plotly.graph_objects as go
+
 
 def create_gauge():
     app = DjangoDash("gauge_test")
 
     app.layout = html.Div(
         [
-            daq.Gauge(id="my-gauge-1", label="Default", value=6),
-            dcc.Slider(id="my-gauge-slider-1", min=0, max=10, step=1, value=5),
+            daq.Gauge(id="my-gauge-1", label="Received Samples", value=96),
+            # dcc.Slider(id="my-gauge-slider-1", min=0, max=10, step=1, value=5),
         ]
     )
-
+    """
     @app.callback(Output("my-gauge-1", "value"), Input("my-gauge-slider-1", "value"))
     def update_output(value):
         return value
+    """
+
+
+def create_medium_gauge():
+    app = DjangoDash("gauge_medium")
+    app.layout = html.Div(
+        fig=go.Figure(
+            go.Indicator(
+                mode="gauge+number",
+                value=270,
+                domain={"x": [0, 1], "y": [0, 1]},
+                title={"text": "Speed"},
+            )
+        )
+    )
+    fig.update_layout(transition_duration=500)
+
+    return fig
