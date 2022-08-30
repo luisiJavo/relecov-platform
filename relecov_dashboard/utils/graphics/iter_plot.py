55f5c0848f2d832f1a2354e0f37085c0b80471c4
luisiJavo <luis.aranda.lillo@gmail.com>
2022-08-30 14:23:59 +0200

    dashboard: graph->mutations in variant searching by lineages (ITER)

--- relecov_dashboard/utils/graphics/iter_plot.py
+++ relecov_dashboard/utils/graphics/iter_plot.py
@@ -1,104 +1,12 @@
-import json
-import os
-from django.conf import settings
 import dash_core_components as dcc
 import dash_html_components as html
 from django_plotly_dash import DjangoDash
 from dash.dependencies import Input, Output
 import dash_bio as dashbio
-import pandas as pd
-
-import urllib.request as urlreq
+from relecov_core.utils.handling_variant import get_variant_data_from_lineages
 
 
-def parse_csv(file_path):
-    """
-    fields => SAMPLE(0), CHROM(1), POS(2), REF(3), ALT(4),
-    FILTER(5), DP(6),  REF_DP(7), ALT_DP(8), AF(9), GENE(10),
-    EFFECT(11), HGVS_C(12), HGVS_P(13), HGVS_P1LETTER(14),
-    CALLER(15), LINEAGE(16)
-    """
-    df = pd.read_csv(file_path, sep=",")
-
-    return df
-
-
-def set_dataframe_needle_plot(df, lineage):
-    """
-    This function receives a python dictionary, a list of selected fields and sets a dataframe from fields_selected_list
-    to represent the graph dataframe structure(dict) { x: [], y: [], domains: [], mutationGroups: [],}
-    """
-    if lineage is None:
-        first_line = df.iloc[0]
-        lineage = first_line.at[0, "LINEAGE"]
-
-    filter_df = df[df.LINEAGE == lineage]
-
-    DOMAINS = [
-        {"name": "orf1a", "coord": "265-13468"},
-        {"name": "orf1b", "coord": "13468-21555"},
-        {"name": "Spike", "coord": "21563-25384"},
-        {"name": "orf3a", "coord": "25393-26220"},
-        {"name": "E", "coord": "26245-26472"},
-        {"name": "M", "coord": "26523-27191"},
-        {"name": "orf6", "coord": "27202-27387"},
-        {"name": "orf7a", "coord": "27394-27759"},
-        {"name": "orf8", "coord": "27894-28259"},
-        {"name": "N", "coord": "28274-29533"},
-        {"name": "orf10", "coord": "29558-29674"},
-    ]
-    plot_df = {
-        "x": filter_df.POS.to_list(),
-        "y": filter_df.AF.to_list(),
-        "domains": DOMAINS,
-        "mutationGroups": filter_df.EFFECT.to_list(),
-    }
-
-    return plot_df
-
-
-def parse_json_file(json_file):
-    """
-    This function loads a json file and returns a python dictionary.
-    """
-    json_parsed = {}
-    # f = open(json_file)
-    with open(json_file) as f:
-        json_parsed["data"] = json.load(f)
-
-    return json_parsed
-
-
-def get_list_of_keys(json_parsed):
-    list_of_keys = list(json_parsed["data"].keys())
-    return list_of_keys
-
-
-def get_list_of_dict_of_lineages_from_long_table(df):
-    """
-    This function receives parsed file from parse_csv().
-    Returns a list of dictionaries of lineages [{"label": "B.1.177.57", "value": "B.1.177.57"}]
-    """
-    unique_lineages = df.LINEAGE.unique()
-    list_of_lineages = []
-    for lin in unique_lineages:
-        list_of_lineages.append({"label": lin, "value": lin})
-
-    return list_of_lineages
-
-
-def create_needle_plot_graph_ITER(lineage):
-    needle_data = os.path.join(
-        settings.BASE_DIR, "relecov_core", "docs", "variants_long_table_last.csv"
-    )
-    dict_of_samples = get_list_of_dict_of_lineages_from_long_table(
-        parse_csv(needle_data)
-    )
-    mdata = set_dataframe_needle_plot(parse_csv(needle_data), lineage)
-
-    data = urlreq.urlopen("https://git.io/needle_PIK3CA.json").read().decode("utf-8")
-
-    mdata = json.loads(data)
+def create_needle_plot_graph_ITER(lineage, mdata):
     app = DjangoDash("needle_ITER")
 
     app.layout = html.Div(
@@ -115,7 +23,7 @@ def create_needle_plot_graph_ITER(lineage):
             "Select a Lineage",
             dcc.Dropdown(
                 id="needleplot-select-lineage",
-                options=dict_of_samples,
+                options=2018185,  # dict_of_samples,
                 clearable=False,
                 multi=False,
                 value=lineage,
@@ -176,24 +84,20 @@ def create_needle_plot_graph_ITER(lineage):
             ),
         ]
     )
-    # import pdb; pdb.set_trace()
 
     @app.callback(
         Output("dashbio-needleplot", "mutationData"),
         Input("needleplot-select-lineage", "value"),
     )
     def update_sample(selected_lineage):
-        print(selected_lineage)
-
-        create_needle_plot_graph_ITER(selected_lineage)
-        mdata = set_dataframe_needle_plot(parse_csv(needle_data), selected_lineage)
-        mutation_data = mdata
-        return mutation_data
+        mdata = get_variant_data_from_lineages("B.1.1.7", "NC_045512")
+        create_needle_plot_graph_ITER(selected_lineage, mdata)
+        # mutation_data = mdata
+        # return mutation_data
 
     @app.callback(
         Output("dashbio-needleplot", "rangeSlider"),
         Input("needleplot-rangeslider", "value"),
     )
     def update_range_slider(range_slider_value):
-        print("valor de range ", range_slider_value)
         return True if range_slider_value else False
