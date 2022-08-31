b09092a0472b35d4f71f4bc180adf38e583fffd3
luisiJavo <luis.aranda.lillo@gmail.com>
2022-08-31 13:45:42 +0200

    dashboard-> mutation table->loads 1 sample OK

--- relecov_dashboard/utils/graphics/mutation_table.py
+++ relecov_dashboard/utils/graphics/mutation_table.py
@@ -17,7 +17,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from django_plotly_dash import DjangoDash
 import dash_table
-from relecov_core.models import VariantAnnotation, VariantInSample
+from relecov_core.models import Effect, Gene, VariantAnnotation, VariantInSample
 
 from relecov_core.utils.handling_variant import (
     # get_if_chromosomes_exists,
@@ -74,44 +74,56 @@ def process_mutation_df(df: pd.DataFrame, renaming_dict: dict = None) -> pd.Data
 
 def generate_table(sample_name):
     # "B.1.1.7", "NC_045512"
+    df = {}
     list_of_hgvs_p = []
+    gene_list = []
+    effect_list = []
+    lineage_list = ["B.1.1.7", "B.1.1.7", "B.1.1.7", "B.1.1.7", "B.1.1.7"]
+    sample_list = []
     chromosome = "NC_045512"
     sample_obj = get_sample_obj_from_sample_name(sample_name=sample_name)
     if sample_obj is not None:
         af = get_alelle_frequency_per_sample(
             sample_name=sample_name, chromosome=chromosome
         )
-        print(af)
         pos = get_position_per_sample(sample_name=sample_name, chromosome=chromosome)
-        print(pos)
-        effect = create_effect_list(sample_name=sample_name, chromosome=chromosome)
-        print(effect)
         variant_in_sample_objs = VariantInSample.objects.filter(sampleID_id=sample_obj)
         for variant_in_sample_obj in variant_in_sample_objs:
-            print(variant_in_sample_obj.get_variantID_id())
             variant_annotation_objs = VariantAnnotation.objects.filter(
                 variantID_id=variant_in_sample_obj.get_variantID_id()
             )
-            print(variant_annotation_objs)
             for variant_annotation_obj in variant_annotation_objs:
-                list_of_hgvs_p.append(variant_annotation_obj.get_hgvs_p())
-        print(list_of_hgvs_p)
+                hgvs_p = variant_annotation_obj.get_variant_in_sample_data()[1]
+                list_of_hgvs_p.append(hgvs_p)
+
+                geneID_id = variant_annotation_obj.get_geneID_id()
+                gene_obj = Gene.objects.filter(gene_name__iexact=geneID_id).last()
+                gene_list.append(gene_obj.get_gene_name())
+
+                effect_obj = Effect.objects.filter(
+                    effect__iexact=variant_annotation_obj.get_effectID_id()
+                ).last()
+                effect_list.append(effect_obj.get_effect())
+
+                sample_list.append(sample_name)
+
+        df["SAMPLE"] = sample_list
+        df["POS"] = pos
+        df["Mutation"] = list_of_hgvs_p
+        df["AF"] = af
+        df["EFFECT"] = effect_list
+        df["GENE"] = gene_list
+        df["LINEAGE"] = lineage_list
+
+        return df
+    else:
+        return None
 
 
 def create_mutation_table(sample):
-    generate_table(sample_name=sample)
+    df = generate_table(sample_name=sample)
+    df_pandas = pd.DataFrame.from_dict(df)
     PAGE_SIZE = 20
-
-    input_file = os.path.join(
-        settings.BASE_DIR, "relecov_core", "docs", "variants_long_table_last.csv"
-    )
-
-    # Read data
-    df = read_mutation_data(input_file, file_extension="csv")
-    df = process_mutation_df(df)
-
-    # Read some extra values
-    effects = list(df["EFFECT"].unique())
 
     app = DjangoDash("mutation_table")
 
@@ -120,18 +132,18 @@ def create_mutation_table(sample):
             html.P(id="mutation_table-message"),
             dcc.Dropdown(
                 id="mutation_table-effect_dropdown",
-                options=[{"label": i, "value": i} for i in effects],
+                options=[{"label": i, "value": i} for i in df["EFFECT"]],
                 clearable=False,
                 multi=True,
-                value=1,
+                value=[{"label": i, "value": i} for i in df["EFFECT"]][0],
                 style={"width": "400px"},
                 placeholder="Mutation effect",
             ),
             html.Br(),
             dash_table.DataTable(
-                id="mutation_table",
-                data=df.to_dict("records"),
-                columns=[{"name": i, "id": i} for i in df.columns],
+                id="mutation_datatable",
+                data=df_pandas.to_dict("records"),
+                columns=[{"name": i, "id": i} for i in df_pandas.columns],
                 page_current=0,
                 page_size=PAGE_SIZE,
                 page_action="custom",
@@ -140,18 +152,19 @@ def create_mutation_table(sample):
     )
 
     @app.callback(
-        Output("mutation_table", "data"),
+        Output("mutation_datatable", "data"),
         Input("mutation_table-effect_dropdown", "value"),
     )
     def update_selected_effects(selected_effects):
-        data = df
+        data = {}
         if type(selected_effects) == list and len(selected_effects) >= 1:
-            data = data[data["EFFECT"].isin(selected_effects)]
-        return data.to_dict("records")
+            data = df_pandas.to_dict("records")
+        #   data = data[data["EFFECT"].isin(df["EFFECT"])]
+        return data
 
     @app.callback(
         Output("mutation_table-message", "children"),
-        Input("mutation_table", "active_cell"),
+        Input("mutation_datatable", "active_cell"),
     )
     def show_clicks(active_cell):
         if active_cell:
@@ -159,5 +172,5 @@ def create_mutation_table(sample):
         else:
             return "Click the table"
 
-    if __name__ == "__main__":
-        app.run_server(debug=True)
+    # if __name__ == "__main__":
+    #     app.run_server(debug=True)
